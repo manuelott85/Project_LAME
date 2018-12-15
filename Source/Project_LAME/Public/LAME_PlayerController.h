2c3a5d917d131ced154f062d19d2ffc389641001
Manuel Ott <email@manuelott.com>
2018-12-15 14:16:45 +0100

    Base Files Init as C++ and Blueprints derivate from them

---
+++ Source/Project_LAME/Public/LAME_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LAME_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LAME_API ALAME_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
