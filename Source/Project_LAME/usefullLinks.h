1c8222f29c36930c231d696e0941b3540310389a
Manuel Ott <email@manuelott.com>
2019-01-24 17:21:37 +0100

    Replication Tests

---
+++ Source/Project_LAME/usefullLinks.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+/*
+Introduction to C++
+https://docs.unrealengine.com/en-US/Programming/Introduction
+
+Epic's coding standart
+https://docs.unrealengine.com/en-US/Programming/Development/CodingStandard
+
+UPROPERTIY
+https://docs.unrealengine.com/en-US/Programming/UnrealArchitecture/Reference/Properties
+
+UFUNCTION
+https://docs.unrealengine.com/en-us/Programming/UnrealArchitecture/Reference/Functions/Specifiers
+
+Logging to console and on screen messages
+https://wiki.unrealengine.com/Logs,_Printing_Messages_To_Yourself_During_Runtime
+
+Gameplay Debugger
+https://docs.unrealengine.com/en-us/Gameplay/Tools/GameplayDebugger
+
+Custom EQS Test
+https://mercuna.com/writing-eqs-test/
+
+Collision Avoidence
+http://gamma.cs.unc.edu/ORCA/publications/ORCA.pdf
+
+Replication and HasAuthority
+https://forums.unrealengine.com/development-discussion/blueprint-visual-scripting/17089-what-is-the-actual-meaning-of-authority
+*/
